5ed759384807693af5ce820939b997399d83a3cc
DavidHughesJr <hughes014@yahoo.com>
2023-02-08 20:43:14 -0600

    services list added

--- base.js
+++ base.js
@@ -5,7 +5,8 @@ const hamburgerMenu = document.querySelector('.hamburger-menu')
 const closeHamburger = document.querySelector('.close-hamburger-menu')
 
 hamburgerMenu.addEventListener('click', () => {
-    mobileNav.classList.toggle('nav-mobile-hidden')
     mobileNav.classList.toggle('nav-mobile-active')
-    
+    if(mobileNav.classList.contains('nav-mobile-active'))
+    mobileNav.classList.remove('nav-mobile-hidden')
+    else mobileNav.classList.add('nav-mobile-hidden')
 })
